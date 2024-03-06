cabb2fadf27ab094418a59a01eac820aae25317b
Jevon Cochran <jevon@securebags.com>
2024-03-06 12:21:20 -0500

    hot fix

--- data/seeds/002_add_banks.js
+++ data/seeds/002_add_banks.js
@@ -4,6 +4,7 @@
  */
 exports.seed = function (knex) {
   return knex("banks").insert([
+    // Colombian Banks
     { id: 1, name: "Bancolombia", country: "CO", isCashPickupSite: true },
     { id: 2, name: "Banco de Bogotá", country: "CO", isCashPickupSite: false },
     { id: 3, name: "Davivienda", country: "CO", isCashPickupSite: true },
@@ -15,6 +16,7 @@ exports.seed = function (knex) {
     },
     { id: 5, name: "BBVA", country: "CO", isCashPickupSite: true },
 
+    // Mexican Banks
     { id: 6, name: "BBVA", country: "MX", isCashPickupSite: true },
     { id: 7, name: "Banorte", country: "MX", isCashPickupSite: true },
     { id: 8, name: "Banco Santander", country: "MX", isCashPickupSite: false },
@@ -25,5 +27,43 @@ exports.seed = function (knex) {
       isCashPickupSite: false,
     },
     { id: 10, name: "HSBC", country: "MX", isCashPickupSite: true },
+
+    // Brazilian Banks
+    { id: 11, name: "Banco do Brasil", country: "BR", isCashPickupSite: false },
+    { id: 12, name: "Itaú Unibanco", country: "BR", isCashPickupSite: false },
+    { id: 13, name: "Banco Santander", country: "BR", isCashPickupSite: false },
+    { id: 14, name: "Banco Bradesco", country: "BR", isCashPickupSite: true },
+    {
+      id: 15,
+      name: "Caixa Econômica Federal",
+      country: "BR",
+      isCashPickupSite: true,
+    },
+
+    // Indian Banks
+    {
+      id: 16,
+      name: "State Bank of India",
+      country: "IN",
+      isCashPickupSite: true,
+    },
+    {
+      id: 17,
+      name: "Bank of Baroda",
+      country: "IN",
+      isCashPickupSite: false,
+    },
+    {
+      id: 18,
+      name: "Canara Bank",
+      country: "IN",
+      isCashPickupSite: true,
+    },
+    {
+      id: 19,
+      name: "Punjab National Bank",
+      country: "IN",
+      isCashPickupSite: true,
+    },
   ]);
 };
--- models/transferModel.js
+++ models/transferModel.js
@@ -4,23 +4,21 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const completeTransfer = async (transfer) => {
-  return (
-    db("transfers")
-      .insert(
-        {
-          ...transfer,
-          sent:
-            process.env.NODE_ENV === "production"
-              ? db.raw("to_timestamp(?)", [transfer.sent / 1000])
-              : transfer.sent,
-        },
-        "id"
-      )
-      .then((ids) => {
-        const [id] = ids;
-        return findTransferBy(id);
-      })
-  );
+  return db("transfers")
+    .insert(
+      {
+        ...transfer,
+        sent:
+          process.env.NODE_ENV === "production"
+            ? db.raw("to_timestamp(?)", [transfer.sent / 1000])
+            : transfer.sent,
+      },
+      "id"
+    )
+    .then((ids) => {
+      const [id] = ids;
+      return findTransferBy(id);
+    });
 };
 
 const findTransferBy = async (filter) => {
